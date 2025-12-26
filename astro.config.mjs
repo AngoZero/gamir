f7c05988877d125c2b351f115d06a0e38210a1e2
AngoZero <angozero99@gmail.com>
2025-12-25 20:17:50 -0600

    [chore]: Mejoras diseño + formulario + SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gamirsalon.com', // Cambia esto por tu dominio real
+  integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
